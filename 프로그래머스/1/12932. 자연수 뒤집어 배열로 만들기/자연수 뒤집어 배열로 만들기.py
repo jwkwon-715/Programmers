abad5c8fe91ed4cf0702497daec584fc88741493
jwkwon-715 <3r453.618@gmail.com>
2025-01-08 20:56:06 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,7 +1,2 @@
 def solution(n):
-    answer = []
-    for i in range(len(str(n))):
-        x = n % 10
-        answer.append(x)
-        n = n // 10
-    return answer
+    return list(map(int, reversed(str(n))))
