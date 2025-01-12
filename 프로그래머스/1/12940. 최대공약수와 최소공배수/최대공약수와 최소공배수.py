a1ff678618d644e71c39ba0ebb243dceae843794
jwkwon-715 <3r453.618@gmail.com>
2025-01-12 22:40:56 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,4 +1,4 @@
-import math
-
 def solution(n, m):
-    return [math.gcd(n, m), (n * m) // math.gcd(n, m)]
+    gcd = lambda a,b : b if not a%b else gcd(b, a%b)
+    lcm = lambda a,b : a*b//gcd(a,b)
+    return [gcd(n, m), lcm(n, m)]
