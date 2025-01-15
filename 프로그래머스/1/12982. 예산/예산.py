a9ee3b0d4781a6291fa6a003c839eed37cac2ddc
jwkwon-715 <3r453.618@gmail.com>
2025-01-15 22:39:23 +0900

    [level 1] Title: 예산, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,10 +1,10 @@
 def solution(d, budget):
     d.sort()
-    answer = []
-    for i in range(len(d)):
-        answer.append(d[i])
-        if sum(answer) > budget:
-            answer.pop()
+    answer = 0
+    for i in d:
+        budget -= i
+        if budget < 0:
             break
+        answer += 1
     
-    return len(answer)
+    return answer
